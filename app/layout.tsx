6b8c6b22da3d14961185b7f5b54548f7a33cbf9b
DuzDeveloper <damianuzcanga@gmail.com>
2026-02-06 17:48:21 -0400

    Metadatas

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'NFT Mint Mini App',
   description: 'Mintea tu NFT gratis en Base',
+  other: {
+    'base:app_id': '698609178dcaa0daf5755fb6',
+  },
 };
 
 export default function RootLayout({
